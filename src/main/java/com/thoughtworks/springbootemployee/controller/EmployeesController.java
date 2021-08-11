64e5746186382e641275ffe438c56cfd6053edad
ADRIAPA <ADRIAPA@OOCL.COM>
2021-08-11 23:23:36 +0800

    [Employee][Cedie]refactor: removed unneccessary comments and imports

--- src/main/java/com/thoughtworks/springbootemployee/controller/EmployeesController.java
+++ src/main/java/com/thoughtworks/springbootemployee/controller/EmployeesController.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/employees")
@@ -20,7 +19,6 @@ public class EmployeesController {
     @GetMapping
     public List<Employee> getAllEmployees(){
         return employeeService.getAllEmployees();
-//        return employees;
     }
 
     @GetMapping(path = "/{employeeId}")
