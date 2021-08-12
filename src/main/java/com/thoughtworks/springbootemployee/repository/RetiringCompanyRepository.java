7e0dd79480625f9b018793284a7fecc771ce88e7
ADRIAPA <ADRIAPA@OOCL.COM>
2021-08-12 18:20:40 +0800

    [Company][Cedie]refactor: migrated company to retiring class for the new implementation

---
+++ src/main/java/com/thoughtworks/springbootemployee/repository/RetiringCompanyRepository.java
@@ -0,0 +1,32 @@
+package com.thoughtworks.springbootemployee.repository;
+
+import com.thoughtworks.springbootemployee.model.Company;
+import com.thoughtworks.springbootemployee.model.Employee;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@Repository
+public class RetiringCompanyRepository {
+    private List<Company> companies = new ArrayList<>();
+
+    public RetiringCompanyRepository() {
+        companies.add(new Company(1,"OOCL", Arrays.asList(
+                new Employee(1,"Lara",21,"female",1000),
+                new Employee(3,"Kitz",22,"male",1000),
+                new Employee(4,"Robert",23,"male",500),
+                new Employee(5,"Kyle",24,"male",1000),
+                new Employee(6,"Angelo",25,"male",500))));
+        companies.add(new Company(2, "DPMCP", Arrays.asList(
+                new Employee(2,"Cedie",27,"male",500),
+                new Employee(7,"Jesse",25,"male",500),
+                new Employee(8,"David",25,"male",500))));
+    }
+
+    public List<Company> getCompanies(){
+        return companies;
+    }
+
+}
---
+++ src/main/java/com/thoughtworks/springbootemployee/service/RetiringCompanyService.java
@@ -0,0 +1,79 @@
+package com.thoughtworks.springbootemployee.service;
+
+import com.thoughtworks.springbootemployee.model.Company;
+import com.thoughtworks.springbootemployee.model.Employee;
+import com.thoughtworks.springbootemployee.repository.RetiringCompanyRepository;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class RetiringCompanyService {
+    @Resource
+    private RetiringCompanyRepository retiringCompanyRepository;
+
+    public RetiringCompanyService(RetiringCompanyRepository retiringCompanyRepository) {
+        this.retiringCompanyRepository = retiringCompanyRepository;
+    }
+
+    public List<Company> getAllCompanies(){
+        return retiringCompanyRepository.getCompanies();
+    }
+
+    public Company findById(Integer companyId) {
+        return getAllCompanies()
+                .stream()
+                .filter(company -> company.getId().equals(companyId))
+                .findFirst()
+                .orElse(null);
+    }
+
+    public List<Employee> getAllEmployeesByCompanyId(Integer companyId) {
+        return findById(companyId).getEmployees();
+    }
+
+    public List<Company> getCompaniesByPagination(Integer pageIndex, Integer pageSize) {
+        return getAllCompanies()
+                .stream()
+                .skip((pageIndex - 1) * pageSize)
+                .limit(pageSize)
+                .collect(Collectors.toList());
+    }
+
+    public Company addCompany(Company companyInfo) {
+        Company newCompany = new Company(retiringCompanyRepository.getCompanies().size()+1,
+                companyInfo.getCompanyName(), companyInfo.getEmployees());
+        retiringCompanyRepository.getCompanies().add(newCompany);
+        return newCompany;
+    }
+
+    public Company updateCompany(Integer companyId, Company companyInfo) {
+        return getAllCompanies()
+                .stream()
+                .filter(company -> company.getId().equals(companyId))
+                .findFirst()
+                .map(company -> updateCompanyInfo(company, companyInfo))
+                .get();
+    }
+
+    public Company removeCompany(Integer companyId) {
+        Company removeCompany = retiringCompanyRepository.getCompanies().stream()
+                .filter(company -> company.getId().equals(companyId))
+                .findFirst().orElse(null);
+        retiringCompanyRepository.getCompanies().remove(removeCompany);
+        return removeCompany;
+    }
+
+    private Company updateCompanyInfo(Company company, Company companyInfo) {
+        if (companyInfo.getCompanyName() != null){
+            company.setCompanyName(companyInfo.getCompanyName());
+        }
+        if (!companyInfo.getEmployees().isEmpty() && companyInfo.getEmployees() != null){
+            company.setEmployees(companyInfo.getEmployees());
+        }
+        return company;
+    }
+
+}
