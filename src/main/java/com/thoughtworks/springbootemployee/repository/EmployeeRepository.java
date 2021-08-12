f6bf7538e869316742726a7cc463e405e740d9a6
ADRIAPA <ADRIAPA@OOCL.COM>
2021-08-12 18:20:08 +0800

    [Employee][Cedie]feat: implemented usage of JPA for employee

--- src/main/java/com/thoughtworks/springbootemployee/repository/EmployeeRepository.java
+++ src/main/java/com/thoughtworks/springbootemployee/repository/EmployeeRepository.java
@@ -1,13 +1,18 @@
-//package com.thoughtworks.springbootemployee.repository;
-//
-//import com.thoughtworks.springbootemployee.model.Employee;
-//import org.springframework.data.jpa.repository.JpaRepository;
-//import org.springframework.stereotype.Repository;
-//
-//import java.util.List;
-//
-//@Repository
-//public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
-//
-////    public List<Employee> find
-//}
+package com.thoughtworks.springbootemployee.repository;
+
+import com.thoughtworks.springbootemployee.model.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+
+    List<Employee> findAll();
+
+    List<Employee> findAllByGender(String gender);
+
+    List<Employee> findAllByAgeBetween(Integer minAge, Integer maxAge);
+
+}
--- src/main/java/com/thoughtworks/springbootemployee/service/EmployeeService.java
+++ src/main/java/com/thoughtworks/springbootemployee/service/EmployeeService.java
@@ -1,97 +1,77 @@
-//package com.thoughtworks.springbootemployee.service;
-//
-//import com.thoughtworks.springbootemployee.model.Employee;
-////import com.thoughtworks.springbootemployee.repository.EmployeeRepository;
+package com.thoughtworks.springbootemployee.service;
+
+import com.thoughtworks.springbootemployee.model.Employee;
+import com.thoughtworks.springbootemployee.repository.EmployeeRepository;
 //import com.thoughtworks.springbootemployee.repository.RetiringEmployeeRepository;
-//import org.springframework.data.jpa.repository.Query;
-//import org.springframework.stereotype.Service;
-//
-//import javax.annotation.Resource;
-//import java.util.List;
-//import java.util.stream.Collectors;
-//
-//@Service
-//public class EmployeeService {
-//    @Resource
-//    private EmployeeRepository employeeRepository;
-//
-//    public EmployeeService(EmployeeRepository employeeRepository) {
-//       this.employeeRepository = employeeRepository;
-//    }
-//
-//    public List<Employee> getAllEmployees() {
-//        return employeeRepository.findAll();
-//    }
-//
-//    public Employee findById(Integer employeeId){
-//        return getAllEmployees()
-//                .stream()
-//                .filter(employee -> employee.getEmployeeId().equals(employeeId))
-//                .findFirst()
-//                .orElse(null)
-//                ;
-//    }
-//
-//    public List<Employee> findByGender(String employeeGender){
-//        return getAllEmployees()
-//                .stream()
-//                .filter(employee -> employee.getEmployeeGender().equals(employeeGender))
-//                .collect(Collectors.toList());
-//    }
-//
-//    public List<Employee> getEmployeesByPagination(Integer pageIndex, Integer pageSize) {
-//        return getAllEmployees()
-//                .stream()
-//                .skip((pageIndex - 1) * pageSize)
-//                .limit(pageSize)
-//                .collect(Collectors.toList());
-//    }
-//
-//    public Employee addEmployee(Employee employeeInfo) {
-//        return employeeRepository.save(employeeInfo);
-////        Employee newEmployee = new Employee(employeeRepository.getEmployees().size()+1,
-////                employeeInfo.getEmployeeName(), employeeInfo.getEmployeeAge(), employeeInfo.getEmployeeGender(), employeeInfo.getEmployeeSalary());
-////        employeeRepository.getEmployees().add(newEmployee);
-////        return newEmployee;
-//    }
-//
-//    public Employee updateEmployee(Integer employeeId, Employee employeeInfo){
-//        return getAllEmployees()
-//                .stream()
-//                .filter(employee -> employee.getEmployeeId().equals(employeeId))
-//                .findFirst()
-//                .map(employee -> updateEmployeeInfo(employee, employeeInfo))
-//                .get()
-//                ;
-//    }
-//
-//    public Employee removeEmployee(Integer employeeId){
-//        Employee removeEmployee = employeeRepository.findAll().stream()
-//                                        .filter(employee -> employee.getEmployeeId()
-//                                        .equals(employeeId))
-//                                        .findFirst().orElse(null);
-//        employeeRepository.findAll().remove(removeEmployee);
-//        return removeEmployee;
-//    }
-//
-//    private Employee updateEmployeeInfo(Employee employee, Employee employeeInfo) {
-//        if (employeeInfo.getEmployeeName() != null){
-//            employee.setEmployeeName(employeeInfo.getEmployeeName());
-//        }
-//        if (employeeInfo.getEmployeeAge() != null){
-//            employee.setEmployeeAge(employeeInfo.getEmployeeAge());
-//        }
-//        if (employeeInfo.getEmployeeGender() != null){
-//            employee.setEmployeeGender(employeeInfo.getEmployeeGender());
-//        }
-//        if (employeeInfo.getEmployeeSalary() != null){
-//            employee.setEmployeeSalary(employeeInfo.getEmployeeSalary());
-//        }
-//        return employee;
-//    }
-//
-//    @Query
-//    public List<Employee> getEmployeesByAgeRange(Integer minAge, Integer maxAge) {
-//        return null;
-//    }
-//}
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@Service
+public class EmployeeService {
+
+    private EmployeeRepository employeeRepository;
+
+    public EmployeeService(EmployeeRepository employeeRepository) {
+       this.employeeRepository = employeeRepository;
+    }
+
+    public Employee addEmployee(Employee employeeInfo) {
+        return employeeRepository.save(employeeInfo);
+    }
+
+    public List<Employee> getAllEmployees() {
+       return employeeRepository.findAll();
+    }
+
+    public Employee findById(Integer employeeId) {
+        return employeeRepository.findById(employeeId).orElse(null);
+    }
+
+    public List<Employee> findByGender(String employeeGender) {
+        return employeeRepository.findAllByGender(employeeGender);
+    }
+
+    public List<Employee> getEmployeesByAgeRange(Integer minAge, Integer maxAge) {
+        return employeeRepository.findAllByAgeBetween(minAge, maxAge);
+    }
+
+    public Employee removeEmployee(Integer employeeId) {
+        Optional<Employee> removeEmployee = employeeRepository.findById(employeeId);
+        employeeRepository.deleteById(employeeId);
+        return removeEmployee.orElse(null);
+    }
+
+    public Employee updateEmployee(Integer employeeId, Employee employeeInfo) {
+        Employee updateEmployee = employeeRepository.findById(employeeId)
+                .map(employee -> updateEmployeeInfo(employee, employeeInfo))
+                .get();
+        return employeeRepository.save(updateEmployee);
+    }
+
+    private Employee updateEmployeeInfo(Employee employee, Employee employeeInfo) {
+        if (employeeInfo.getName() != null){
+            employee.setName(employeeInfo.getName());
+        }
+        if (employeeInfo.getAge() != null){
+            employee.setAge(employeeInfo.getAge());
+        }
+        if (employeeInfo.getGender() != null){
+            employee.setGender(employeeInfo.getGender());
+        }
+        if (employeeInfo.getSalary() != null){
+            employee.setSalary(employeeInfo.getSalary());
+        }
+        return employee;
+    }
+
+    public List<Employee> getEmployeesByPagination(Integer pageIndex, Integer pageSize) {
+        return employeeRepository.findAll(PageRequest.of(pageIndex-1,pageSize)).getContent();
+    }
+
+}
