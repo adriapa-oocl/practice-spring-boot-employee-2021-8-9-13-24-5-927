2ce6d86f6fd39c49c49cf47d9312b50ad87d79bf
ADRIAPA <ADRIAPA@OOCL.COM>
2021-08-12 18:23:33 +0800

    [Employee][Cedie]test: test cases for search/add/update/delete api for employee

--- src/test/java/com/thoughtworks/springbootemployee/integration/EmployeeIntegrationTest.java
+++ src/test/java/com/thoughtworks/springbootemployee/integration/EmployeeIntegrationTest.java
@@ -7,6 +7,7 @@ import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
@@ -39,4 +40,59 @@ public class EmployeeIntegrationTest {
 
         //then
     }
+
+    @Test
+    void should_create_when_addEmployee_given_employee_information() throws Exception {
+        //given
+        String employee = "{\n" +
+                "    \"id\": 98,\n" +
+                "    \"name\": \"Joanna\",\n" +
+                "    \"age\": 25,\n" +
+                "    \"gender\": \"female\",\n" +
+                "    \"salary\": 1000\n" +
+                "}";
+
+        //when
+        mockMvc.perform(MockMvcRequestBuilders.post("/employees")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(employee))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.name").value("Joanna"))
+                .andExpect(jsonPath("$.gender").value("female"))
+                .andExpect(jsonPath("$.salary").value("1000"));
+
+    }
+
+    @Test
+    void should_update_when_updateEmployee_given_employee_information() throws Exception {
+        //given
+        final Employee employee = new Employee(95, "Joanna", 25, "female", 1000);
+        final Employee savedEmployee = employeeRepository.save(employee);
+        String newEmployeeInfo = "{\n" +
+                "    \"age\": 22\n" +
+                "}";
+
+        //when
+        int id = savedEmployee.getId();
+        mockMvc.perform(MockMvcRequestBuilders.put("/employees/{id}", id)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(newEmployeeInfo))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.age").value("22"));
+    }
+
+    @Test
+    void should_remove_when_removeEmployee_given_employee_id() throws Exception {
+        //given
+        final Employee employee = new Employee(95, "Joanna", 25, "female", 1000);
+        final Employee savedEmployee = employeeRepository.save(employee);
+
+        //when
+        int id = savedEmployee.getId();
+        mockMvc.perform(MockMvcRequestBuilders.delete("/employees/{id}", id)
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+    }
+
 }
