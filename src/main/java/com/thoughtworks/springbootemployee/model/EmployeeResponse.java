8f1ddb5577f45b39f9ef412e1713d1484fae1020
ADRIAPA <ADRIAPA@OOCL.COM>
2021-08-13 15:10:48 +0800

    [Employee][Cedie]feat: implemented request and response for output of employee

---
+++ src/main/java/com/thoughtworks/springbootemployee/model/EmployeeResponse.java
@@ -0,0 +1,59 @@
+package com.thoughtworks.springbootemployee.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+public class EmployeeResponse {
+    private String name;
+    private Integer age;
+    private String gender;
+    private Integer salary;
+
+    public EmployeeResponse(String name, Integer age, String gender, Integer salary) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+        this.salary = salary;
+    }
+
+    public EmployeeResponse() {
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public Integer getSalary() {
+        return salary;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public void setSalary(Integer salary) {
+        this.salary = salary;
+    }
+
+}
+
+
