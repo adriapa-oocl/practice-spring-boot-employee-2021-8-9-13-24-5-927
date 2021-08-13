1e6fa194dc036df4d197c4d580d588ce8260d538
ADRIAPA <ADRIAPA@OOCL.COM>
2021-08-13 14:58:08 +0800

    [Employee][Cedie]feat: implemented exception handling for employee

--- src/main/java/com/thoughtworks/springbootemployee/exception/EmployeeNotFoundException.java
+++ src/main/java/com/thoughtworks/springbootemployee/exception/EmployeeNotFoundException.java
@@ -1,14 +1,15 @@
 package com.thoughtworks.springbootemployee.exception;
 
 public class EmployeeNotFoundException extends RuntimeException {
-    private String message;
 
-    public EmployeeNotFoundException(String message) {
-        this.message = message;
-    }
+//    private String message;
+//
+//    public EmployeeNotFoundException(String message){
+//        super(message);
+//    }
 
     @Override
     public String getMessage() {
-        return message;
+        return "Employee id not found";
     }
 }
